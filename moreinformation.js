e1e8276bcfe42818f44e8bbbf1fea38570ad0305
AadhilMawanella <112420417+AadhilMawanella@users.noreply.github.com>
2022-10-12 14:04:36 +0530

    Add files via upload

--- moreinformation.js
+++ moreinformation.js
@@ -1,12 +1,15 @@
 function clic(){
     var x = document.getElementById("div");
-    if(x.style.display === "block"){
-       x.style.display = "none"
+    var y=document.getElementById("msm");
+    if(y.style.display==="block" && x.style.display === "block"){
+       x.style.display = "none";
+       y.style.display="none";
        document.getElementById("uom").innerHTML="more information";
        
     }else
     {
-       x.style.display = "block"
+       x.style.display = "block";
+       y.style.display="block";
        document.getElementById("uom").innerHTML="If you haven't anything press again";
     }
 }
